6acfd4d1d76b7410e81c0bba83684674baa4d097
MingboLi <mingboli@outlook.com>
2023-06-17 22:45:35 +0800

    fix:update sh file to run well

--- docs/update_po.sh
+++ docs/update_po.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-make gettext
-sphinx-intl update -p _build/gettext -l zh_CN
-
+#!/bin/bash
+mkdir -p _build/gettext &&
+make gettext &&
+sphinx-intl update -p _build/gettext -l zh_CN &&
 echo "po files has been updated. Please update po files in locales folder."
