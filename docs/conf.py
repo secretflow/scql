d54c27fa5bc0c20c7cc5f6f9e816e88458fbf68a
Tony Wu <tonywu6@protonmail.com>
2025-06-05 11:05:46 +0800

    chore: update docs config

--- docs/conf.py
+++ docs/conf.py
@@ -15,16 +15,21 @@
 project = "SCQL"
 
 extensions = [
+    # enable support for .md files
+    # https://myst-parser.readthedocs.io/en/latest/
     "myst_parser",
     "secretflow_doctools",
+    # link to titles using :ref:`Title text`
+    # https://www.sphinx-doc.org/en/master/usage/extensions/autosectionlabel.html
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.extlinks",
     "sphinx.ext.graphviz",
-    "sphinx.ext.mathjax",
     "sphinx.ext.todo",
     "sphinxcontrib.mermaid",
 ]
 
+# also link to titles using :ref:`path/to/document:Title text`
+# (note that path should not have a leading slash)
 # https://www.sphinx-doc.org/en/master/usage/extensions/autosectionlabel.html#confval-autosectionlabel_prefix_document
 autosectionlabel_prefix_document = True
 
@@ -39,6 +44,8 @@ gettext_uuid = False
 # otherwise any change to source text will cause their translations to not appear
 gettext_allow_fuzzy_translations = True
 
+# list of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
 exclude_patterns = [
     "CONTRIBUTING.md",  # prevent CONTRIBUTING.md from being included in output, optional
     ".venv",
@@ -51,22 +58,39 @@ todo_include_todos = True
 
 # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
 myst_enable_extensions = [
+    # LaTeX math
+    # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#direct-latex-math
     "amsmath",
+    # attributes
+    # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#attributes
+    "attrs_block",
+    "attrs_inline",
+    # code fence using :::
+    # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#code-fences-using-colons
     "colon_fence",
-    "deflist",
+    # $math$ and $$math$$
+    # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#dollar-delimited-math
     "dollarmath",
+    # :name: value
+    # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#field-lists
     "fieldlist",
-    "html_admonition",
+    # <img src="...">
+    # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#html-images
     "html_image",
+    # detect "bare" links
+    # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#linkify
     "linkify",
-    "replacements",
+    # "double quotes" => “double quotes”
+    # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#typography
     "smartquotes",
+    # ~~strikethrough~~
+    # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#strikethrough
     "strikethrough",
-    "substitution",
-    "tasklist",
 ]
+# enable all MyST syntax features
 # https://myst-parser.readthedocs.io/en/latest/configuration.html#global-configuration
 myst_gfm_only = False
+# generate #anchors for heading # through ######
 # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#auto-generated-header-anchors
 myst_heading_anchors = 6
 
